74ed38dd7b3e30d4854cbd4181987aeec72f262a
bvelosofioretti <bvelosofioretti@nautilus6.danielcastelao.org>
2017-10-23 10:20:01 +0200

    first commit

---
+++ Assets/scripts/enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class enemy : MonoBehaviour {
+	public Transform player;
+	public float distancia;
+	public Text texto;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+
+	void Awake(){
+		agent = GetComponent<NavMeshAgent> ();// a agent le pasamos el navmesh
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		distancia = Vector3.Distance (player.position, transform.position);//calcula distancia
+
+		if(distancia <60){
+		
+			agent.SetDestination (player.position);//
+
+		if (distancia<1.2){
+		
+		texto.text="YOU LOSE";
+				Destroy (GameObject.FindGameObjectWithTag ("Player"));
+			
+	}
+}
+	}
+}
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+ 	public Text countText;
+   	public Text winText;	
+	private Rigidbody rb;
+	private int count;
+	public float speed;
+	void Start()
+	{	
+        rb=GetComponent<Rigidbody>();
+	count = 0;
+        SetCountText ();
+        winText.text = "";
+	}
+		
+	void FixedUpdate() 
+	{
+		float horizontal=Input.GetAxis("Horizontal");
+		float vertical=Input.GetAxis("Vertical");
+		
+		Vector3 movement= new Vector3(vertical,0.0f,horizontal);
+		rb.AddForce(movement * speed);
+	}
+		
+	/* Adaptacion para el acelerometro del movil */
+	/*
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
+		transform.position += new Vector3 (moveHorizontal, 0.0f, moveVertical)* speed * Time.deltaTime;
+	}
+	*/
+	void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Pick up"))
+        {
+            other.gameObject.SetActive (false);
+	    count = count + 1;
+            SetCountText ();
+        }
+    }
+	void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 9)
+        {
+			Destroy (GameObject.FindGameObjectWithTag ("enemigo"));
+            winText.text = "You Win!";
+        }
+    }
+
+}
+
+
